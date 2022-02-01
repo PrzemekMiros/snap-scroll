e695b66a29f8657b844c21c4ed865e4a76de8b3a
PrzemekMiros <62024827+PrzemekMiros@users.noreply.github.com>
2022-02-01 13:15:06 +0100

    Add files via upload

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,5 +1,59 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.from(".hero .line-l-1", {
+  scrollTrigger: {
+    scroller: ".scroll-wrapper",
+    trigger: ".hero",
+    start: 'center 55%',
+    toggleActions: 'play complete restart reverse'
+  }, 
+  x: -100,
+  opacity: 0,
+  duration: 1,
+  ease: ' Power2.easeOut'
+});
+
+gsap.from(".hero .line-l-2", {
+  scrollTrigger: {
+    scroller: ".scroll-wrapper",
+    trigger: ".hero",
+    start: 'center 55%',
+    toggleActions: 'play complete restart reverse'
+  }, 
+  x: 100,
+  opacity: 0,
+  duration: 1,
+  ease: ' Power2.easeOut'
+});
+
+gsap.from(".hero__content h1, .hero__content span, .hero__content p", {
+  scrollTrigger: {
+    scroller: ".scroll-wrapper",
+    trigger: ".hero",
+    start: 'center 55%',
+    toggleActions: 'play complete restart reverse'
+  }, 
+  opacity: 0,
+  duration: 1,
+  stagger: .3,
+  ease: ' Power2.easeOut'
+});
+
+gsap.from(".hero .line", {
+  scrollTrigger: {
+    scroller: ".scroll-wrapper",
+    trigger: ".hero",
+    start: 'center 55%',
+    toggleActions: 'play complete restart reverse'
+  }, 
+  x: 100,
+  opacity: 0,
+  delay: 2,
+  duration: 1,
+  ease: ' Power2.easeOut'
+});
+
+
 gsap.from(".about .fadeIn", {
   scrollTrigger: {
     scroller: ".scroll-wrapper",
